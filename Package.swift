481b5ae61b287278fe9a8940daee8e4cfdd7e411
Falco Danny (CORP) <danny.falco@adp.com>
2018-05-16 23:46:58 -0400

    Vapor 2 Sample

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporSample",
+    name: "nerdserver",
     targets: [
         Target(name: "App"),
         Target(name: "Run", dependencies: ["App"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 2)
     ],
     exclude: [
         "Config",
